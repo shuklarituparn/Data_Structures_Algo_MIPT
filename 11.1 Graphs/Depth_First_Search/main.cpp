26b8d9b5fdcc035d38941c9512830fef9f7f2569
shukla rituparn <shukla.r@phystech.edu>
2023-11-15 12:23:00 +0300

    Added code for DFS

--- 11.1 Graphs/Depth_First_Search/main.cpp
+++ 11.1 Graphs/Depth_First_Search/main.cpp
@@ -9,27 +9,59 @@
 class Graph{
 private:
     int NumberOfVertices_;
-    std::list<int> adjList_; //adjacent list
+    std::list<int> * adjList_; //adjacent list that is a pointer holding info for list type
+    bool * Visited_; //we also a bool array that stores that if the node is visited
 
 public:
     Graph(int NumberOfVertices);
     void addNewEdge(int source, int destination);
-    void DFS(int vertices);
+    void DFS(int vertex);
     ~Graph();
 
 };
 
 Graph::Graph(int NumberOfVertices){
     this->NumberOfVertices_ = NumberOfVertices;
-    std::list
+    adjList_ = new std::list<int> [NumberOfVertices_];
+    Visited_=new bool [NumberOfVertices_];
 
 }
 void Graph::addNewEdge(int source, int destination){
-
+        adjList_[source].push_back(destination);
 }
-void Graph::DFS(int vertices){
+void Graph::DFS(int vertex){ //we are given a vertex from which we are calculating the DFS of the graph
+    Visited_[vertex]= true; //marking that vertex as visited
+    std::cout<<vertex<<" "; //printing the current vertex
+
+    //now taking the help of iterator to iterate over the list
+
+    std::list<int>::iterator it;  //list iterator
+
+    for (it=adjList_[vertex].begin();it!=adjList_[vertex].end(); it++) {   //using the iterator to loop to the adjlist
+        //std::cout<<*it;
+        if (!Visited_[*it]){   //using the iterator to recursively call the function if not visited
+            DFS(*it);
+        }
+    }
+
+
 
 }
 Graph::~Graph(){
+    delete [] adjList_;
+    delete [] Visited_;
 
+}
+
+int main(){
+    Graph graph1(4);
+    graph1.addNewEdge(0,1);
+    graph1.addNewEdge(0,2);
+    graph1.addNewEdge(1,2);
+    graph1.addNewEdge(2,0);
+    graph1.addNewEdge(2,3);
+    graph1.addNewEdge(3,3);
+
+    graph1.DFS(2);
+    return 0;
 }
---
+++ 11.1 Graphs/Topological sort/main.cpp
@@ -0,0 +1,6 @@
+//
+// Created by rituparn on 15.11.23.
+//
+
+
+#i
